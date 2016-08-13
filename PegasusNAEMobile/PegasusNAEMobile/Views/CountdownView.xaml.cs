3bb0bda2b082561d76efb994ccecc0dd4dd73413
asgangal <ashish.gangal@outlook.com>
2016-08-12 23:03:12 -0700

    Laying out the inital views.

---
+++ PegasusNAEMobile/PegasusNAEMobile/Views/CountdownView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PegasusNAEMobile.Views
+{
+    public partial class CountdownView : ContentPage
+    {
+        public CountdownView()
+        {
+            InitializeComponent();
+        }
+    }
+}
