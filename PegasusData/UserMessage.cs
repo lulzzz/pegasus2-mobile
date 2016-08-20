dff24eddf36f0fa2e35fd34b5b0dbf3daee749a9
asgangal <ashish.gangal@outlook.com>
2016-08-20 15:38:43 -0700

    Adding new class files based on the Telemetry data.

---
+++ PegasusData/UserMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PegasusData
+{
+    [JsonObject]
+    public class UserMessage
+    {
+        public UserMessage()
+        {
+
+        }
+
+        public static UserMessage Load(byte[] message)
+        {
+            return JsonConvert.DeserializeObject<UserMessage>(Encoding.UTF8.GetString(message, 0, message.Length - 1));
+        }
+
+        public static UserMessage Load(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<UserMessage>(jsonString);
+        }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
