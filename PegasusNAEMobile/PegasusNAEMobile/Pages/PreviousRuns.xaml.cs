972dd4c9d00b3499eb01f6d22771ac0e6156358f
asgangal <ashish.gangal@outlook.com>
2016-08-28 15:22:45 -0700

    Added new page that will display a list of previous runs.

---
+++ PegasusNAEMobile/PegasusNAEMobile/Pages/PreviousRuns.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PegasusNAEMobile
+{
+    public partial class PreviousRuns : ContentPage
+    {
+        public PreviousRuns()
+        {
+            InitializeComponent();
+        }
+    }
+}
