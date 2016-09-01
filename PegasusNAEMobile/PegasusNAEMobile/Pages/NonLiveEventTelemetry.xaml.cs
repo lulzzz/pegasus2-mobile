8196c2234c9f9c615d810fab6aa0c6c203a6e642
asgangal <ashish.gangal@outlook.com>
2016-09-01 14:08:20 -0700

    Building up the non live telemetry page.

--- PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
+++ PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PegasusData;
 using Xamarin.Forms;
 
 namespace PegasusNAEMobile
@@ -13,6 +13,25 @@ namespace PegasusNAEMobile
         public NonLiveEventTelemetry()
         {
             InitializeComponent();
+            vehicle.Source = Device.OnPlatform(
+            iOS: ImageSource.FromFile("pegasus_vehicle.png"),
+            Android: ImageSource.FromFile("pegasus_vehicle.png"),
+            WinPhone: ImageSource.FromFile("Assets/pegasus_vehicle.png"));
+            System.Diagnostics.Debug.WriteLine(Constants.ScreenHeight + ", " + Constants.ScreenWidth);
+            if (Constants.ScreenHeight < 600)
+            {
+                vehicle.Source = Device.OnPlatform(
+            iOS: ImageSource.FromFile("pegasus_vehicle_small.png"),
+            Android: ImageSource.FromFile("pegasus_vehicle_small.png"),
+            WinPhone: ImageSource.FromFile("Assets/pegasus_vehicle_small.png"));
+            }
         }
+
+        private void PlayPauseButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+       
     }
 }
