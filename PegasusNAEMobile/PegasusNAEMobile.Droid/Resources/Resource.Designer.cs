fb3a4bfa22bf6351eac6a13789522fdab1c792d5
asgangal <ashish.gangal@outlook.com>
2016-09-01 14:10:20 -0700

    Added Azure Mobile Engagement package to Android

--- PegasusNAEMobile/PegasusNAEMobile.Droid/Resources/Resource.Designer.cs
+++ PegasusNAEMobile/PegasusNAEMobile.Droid/Resources/Resource.Designer.cs
@@ -2200,11 +2200,23 @@ namespace PegasusNAEMobile.Droid
 			// aapt resource value: 0x7f0200a0
 			public const int mr_ic_play_light = 2130837664;
 			
-			// aapt resource value: 0x7f0200a2
-			public const int notification_template_icon_bg = 2130837666;
+			// aapt resource value: 0x7f0200a6
+			public const int notification_template_icon_bg = 2130837670;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int pegasus_herobackground = 2130837665;
+			public const int Pause = 2130837665;
+			
+			// aapt resource value: 0x7f0200a2
+			public const int pegasus_herobackground = 2130837666;
+			
+			// aapt resource value: 0x7f0200a3
+			public const int pegasus_vehicle = 2130837667;
+			
+			// aapt resource value: 0x7f0200a4
+			public const int pegasus_vehicle_small = 2130837668;
+			
+			// aapt resource value: 0x7f0200a5
+			public const int Play = 2130837669;
 			
 			static Drawable()
 			{
