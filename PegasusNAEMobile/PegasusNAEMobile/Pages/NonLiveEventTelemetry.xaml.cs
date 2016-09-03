d7d89c63012ab78bf8b761a70f90d254e92845da
asgangal <ashish.gangal@outlook.com>
2016-09-03 15:20:54 -0700

    Setting up auto play and pause.

--- PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
+++ PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
@@ -6,32 +6,159 @@ using System.Threading.Tasks;
 using PegasusData;
 using Xamarin.Forms;
 using PegasusNAEMobile.Collections;
+using System.Threading;
+
 namespace PegasusNAEMobile
 {
     public partial class NonLiveEventTelemetry : ContentPage
     {
         private RootObjectOnboardTelemetry ronboardtelem;
+        private int currenttelemetrypos;   // used to keep track of what telemetry has been displayed and what's left
+        CancellationTokenSource cancellation;  // Used to check if Timer cancellation has been requested.
+        private bool PlayPauseIcon; // TRUE - Display Pause Icon , FALSE - Display Play Icon  
         public NonLiveEventTelemetry()
         {
             InitializeComponent();
+            PlayPauseIcon = true; //Pause Icon is default
             ronboardtelem = new RootObjectOnboardTelemetry();
+            currenttelemetrypos = 0;
+            cancellation = new CancellationTokenSource();
+            if (Device.OS == TargetPlatform.iOS)
+            {
+                PlayPauseButton.Image = "Pause.png";
+            }
+            else if (Device.OS == TargetPlatform.Android)
+            {
+                PlayPauseButton.Image = "Pause.png";
+            }
+            else
+            {
+                PlayPauseButton.Image = "Assets/Pause.png";
+            }
             vehicle.Source = Device.OnPlatform(
             iOS: ImageSource.FromFile("pegasus_vehicle_small.png"),
             Android: ImageSource.FromFile("pegasus_vehicle_small.png"),
             WinPhone: ImageSource.FromFile("Assets/pegasus_vehicle_small.png"));
+            TelemetrySlider.PropertyChanged += TelemetrySlider_PropertyChanged;
+            TelemetrySlider.ValueChanged += TelemetrySlider_ValueChanged;
+            TelemetrySlider.BindingContext = currenttelemetrypos;
             System.Diagnostics.Debug.WriteLine(Constants.ScreenHeight + ", " + Constants.ScreenWidth);    
+        }
+
+        private void TelemetrySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            //System.Diagnostics.Debug.WriteLine("Value Changed");
+        }
+
+        private void TelemetrySlider_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            //System.Diagnostics.Debug.WriteLine("Property Changed : " + e.PropertyName);
         }
 
         protected async override void OnAppearing()
         {
             string onboardtelemetry = await App.Instance.GetLaunchInfoAsync();
-            RootObjectOnboardTelemetry rtelem = OnboardTelemetryCollection.DataDeserializer(onboardtelemetry);
-            System.Diagnostics.Debug.WriteLine(rtelem.collection.Count);
+            ronboardtelem = OnboardTelemetryCollection.DataDeserializer(onboardtelemetry);
+            TelemetrySlider.Minimum = 0;
+            TelemetrySlider.Maximum = ronboardtelem.collection.Count;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, (ronboardtelem.collection.Count * 500));
+            TotalTelemetryPlaybackTime.Text = duration.ToString("g");
+            TimeSpan T = TimeSpan.FromSeconds(0.5);
+            UpdateNonLiveTelemetryUI(T);
+            //System.Diagnostics.Debug.WriteLine(rtelem.collection.Count);
             base.OnAppearing();
         }
+
         private void PlayPauseButton_Clicked(object sender, EventArgs e)
         {
+            if (PlayPauseIcon)   // Pause is displayed. So Pause the playback
+            {
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    PlayPauseButton.Image = "Play.png";
+                }
+                else if (Device.OS == TargetPlatform.Android)
+                {
+                    PlayPauseButton.Image = "Play.png";
+                }
+                else
+                {
+                    PlayPauseButton.Image = "Assets/Play.png";
+                }
+                cancellation.Cancel();
+                PlayPauseIcon = false;   // Because now play icon is diplayed
+            }
+            else   // Play icon is displayed. Set it to Pause and start the playback.
+            {
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    PlayPauseButton.Image = "Pause.png";
+                }
+                else if (Device.OS == TargetPlatform.Android)
+                {
+                    PlayPauseButton.Image = "Pause.png";
+                }
+                else
+                {
+                    PlayPauseButton.Image = "Assets/Pause.png";
+                }
+                PlayPauseIcon = true;
+                cancellation = new CancellationTokenSource();
+                TimeSpan T = TimeSpan.FromSeconds(0.5);
+                UpdateNonLiveTelemetryUI(T);
+            }
+        }
+        protected override void OnDisappearing()
+        {
+            //Interlocked.Exchange(ref this.cancellation, new CancellationTokenSource()).Cancel();
+            cancellation.Cancel();
+            base.OnDisappearing();
+        }
+        /// <summary>
+        /// Callback method for the Timer. Updates the UI every T seconds.
+        /// </summary>
+        private void UpdateNonLiveTelemetryUI(TimeSpan T)
+        {
+            Device.StartTimer(T, () =>
+            {
+                CancellationTokenSource cts = cancellation;
+                if (cts.IsCancellationRequested)
+                {
+                    System.Diagnostics.Debug.WriteLine("Timer cancellation requested");
+                    return false;
+                }
+                if (currenttelemetrypos < ronboardtelem.collection.Count)
+                {
+                    System.Diagnostics.Debug.WriteLine(currenttelemetrypos);
+                    var currenttelemetry = ronboardtelem.collection[currenttelemetrypos];
+                    RunTimeStampNonLive.Text = String.Format("{0:MMMM dd, yyyy}", currenttelemetry.timestamp);
+                    SpeedKPHNonLive.Text = RoundToDecimalPlaces(currenttelemetry.AirSpeedKph);
+                    SteeringBoxPositionNonLive.Text = RoundToDecimalPlaces(currenttelemetry.SteeringBoxPositionDegrees);
+                    NoseWeightNonLive.Text = RoundToDecimalPlaces(currenttelemetry.NoseWeightLbf);
+                    //SideToSide.Text = (currenttelemetry.)
+                    RearLeft.Text = RoundToDecimalPlaces(currenttelemetry.LeftRearWeightLbf);
+                    RearRight.Text = RoundToDecimalPlaces(currenttelemetry.RightRearWeightLbf);
+                    TelemetrySlider.Value = currenttelemetrypos;
+                    
+                    //MaxAccl.Text = (currenttelemetry.acc)
+                    currenttelemetrypos++;
+                    return true;                    
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Stopping Timer");
+                    return false;
+                }
+            });
             
-        }       
+        }
+
+        private string RoundToDecimalPlaces(double val)
+        {
+            double round = (Math.Round((double)val, 2 , MidpointRounding.AwayFromZero));
+            return (String.Format("{0:0.00s}", round));
+        }
     }
 }
