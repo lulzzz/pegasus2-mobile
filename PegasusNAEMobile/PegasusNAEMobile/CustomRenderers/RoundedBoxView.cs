f1c29c4574be0a8d791453762432db638f0f4a49
asgangal <ashish.gangal@outlook.com>
2016-09-02 17:48:18 -0700

    Renderer for circle

---
+++ PegasusNAEMobile/PegasusNAEMobile/CustomRenderers/RoundedBoxView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PegasusNAEMobile
+{ 
+    public class RoundedBoxView : BoxView
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create<RoundedBoxView, double>(p => p.CornerRadius, 0);
+        public double CornerRadius
+        {
+            get { return (double)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+    }
+}
