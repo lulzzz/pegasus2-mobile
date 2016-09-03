05308c467743705405d65ce1146c427d5803af3e
asgangal <ashish.gangal@outlook.com>
2016-09-03 10:54:29 -0700

    downloading the telemetry file when the page appears.

--- PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
+++ PegasusNAEMobile/PegasusNAEMobile/Pages/NonLiveEventTelemetry.xaml.cs
@@ -5,33 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using PegasusData;
 using Xamarin.Forms;
-
+using PegasusNAEMobile.Collections;
 namespace PegasusNAEMobile
 {
     public partial class NonLiveEventTelemetry : ContentPage
     {
+        private RootObjectOnboardTelemetry ronboardtelem;
         public NonLiveEventTelemetry()
         {
             InitializeComponent();
+            ronboardtelem = new RootObjectOnboardTelemetry();
             vehicle.Source = Device.OnPlatform(
             iOS: ImageSource.FromFile("pegasus_vehicle_small.png"),
             Android: ImageSource.FromFile("pegasus_vehicle_small.png"),
             WinPhone: ImageSource.FromFile("Assets/pegasus_vehicle_small.png"));
-            System.Diagnostics.Debug.WriteLine(Constants.ScreenHeight + ", " + Constants.ScreenWidth);
-            if (Constants.ScreenHeight < 600)
-            {
-                vehicle.Source = Device.OnPlatform(
-            iOS: ImageSource.FromFile("pegasus_vehicle_small.png"),
-            Android: ImageSource.FromFile("pegasus_vehicle_small.png"),
-            WinPhone: ImageSource.FromFile("Assets/pegasus_vehicle_small.png"));
-            }
+            System.Diagnostics.Debug.WriteLine(Constants.ScreenHeight + ", " + Constants.ScreenWidth);    
         }
 
+        protected async override void OnAppearing()
+        {
+            string onboardtelemetry = await App.Instance.GetLaunchInfoAsync();
+            RootObjectOnboardTelemetry rtelem = OnboardTelemetryCollection.DataDeserializer(onboardtelemetry);
+            System.Diagnostics.Debug.WriteLine(rtelem.collection.Count);
+            base.OnAppearing();
+        }
         private void PlayPauseButton_Clicked(object sender, EventArgs e)
         {
             
-        }
-
-       
+        }       
     }
 }
