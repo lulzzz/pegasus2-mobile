3c25baa1403ead45a39e34c2ac103817d5c40392
asgangal <ashish.gangal@outlook.com>
2016-09-08 09:28:36 -0700

    Added a few more properties to the renderer

--- PegasusNAEMobile/PegasusNAEMobile/CustomRenderers/RoundedBoxView.cs
+++ PegasusNAEMobile/PegasusNAEMobile/CustomRenderers/RoundedBoxView.cs
@@ -15,5 +15,20 @@ namespace PegasusNAEMobile
             get { return (double)GetValue(CornerRadiusProperty); }
             set { SetValue(CornerRadiusProperty, value); }
         }
+
+        public static readonly BindableProperty StrokeProperty = BindableProperty.Create<RoundedBoxView, Color>(p => p.Stroke, Color.Transparent);
+        public Color Stroke
+        {
+            get { return (Color)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static readonly BindableProperty StrokeThickProperty = BindableProperty.Create<RoundedBoxView, double>(p => p.StrokeThick, default(double));
+
+        public double StrokeThick
+        {
+            get { return (double)GetValue(StrokeThickProperty); }
+            set { SetValue(StrokeThickProperty, value); }
+        }
     }
 }
